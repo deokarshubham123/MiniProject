5e7fe1bb50b9ed75522283cc8654cccb79a5fffe
deokarshubham123 <88767585+deokarshubham123@users.noreply.github.com>
2022-06-22 09:58:13 +0530

    Add files via upload

---
+++ match_mismatch/Comparing1.java
@@ -0,0 +1,52 @@
+package match_mismatch;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Comparing1 {
+	
+	public static void main(String[] args) throws IOException
+	{
+	BufferedReader reader1 = new BufferedReader(new FileReader("C:\\Users\\shudeoka\\SeleniumDemo1\\DemoProject\\src\\match_mismatch\\file1.txt"));
+	BufferedReader reader2 = new BufferedReader(new FileReader("C:\\Users\\shudeoka\\SeleniumDemo1\\DemoProject\\src\\match_mismatch\\file2.txt"));
+
+	String line1 = reader1.readLine();
+	String line2 = reader2.readLine();
+	
+	boolean areEqual = true;
+
+	int lineNum = 1;
+
+	while (line1 != null || line2 != null)
+	{
+	if(line1 == null || line2 == null)
+	{
+	areEqual = false;
+	break;
+	}
+	else if(! line1.equalsIgnoreCase(line2))
+	{
+	areEqual = false;
+	break;
+	}
+
+	line1 = reader1.readLine();
+	line2 = reader2.readLine();
+	lineNum++;
+	}
+	if(areEqual)
+	{
+	System.out.println("Two files have same content.");
+	}
+	else
+	{
+	System.out.println("Two files have different content. They differ at line "+lineNum);
+	System.out.println("File1 has "+line1+" and File2 has "+line2+" at line "+lineNum);
+	}
+
+	reader1.close();
+	reader2.close();
+	}
+
+}
